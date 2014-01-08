7407767fd6b18ce9a1415ca2dcfbcf9ab8830223
Julie Kang <julie.kang@stanfordalumni.org>
2014-01-08 14:21:40 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JournalApp::Application.config.secret_token = '849a4b272ba5aa90d92db3019fbe5ca619c4ee5d81e09c669b090e47aa5caaa56d23e10150772a12e3deda4f0e95dee2c6b3ac9316b9a2aa929add42bf52c57d'
